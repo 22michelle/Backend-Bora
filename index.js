fbeac51098b544a5a5c39f151a0c30d16caed0d7
Michelle <michellemejiaparra4@gmail.com>
2024-07-30 15:08:13 -0500

    Bora

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
